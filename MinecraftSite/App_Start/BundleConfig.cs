8fd46cf01ec9166d09b8b882b022436175896e15
Jeremy <waddledoocanto@gmail.com>
2016-04-07 10:57:55 -0700

    Added custom header render section and image gallery

--- MinecraftSite/App_Start/BundleConfig.cs
+++ MinecraftSite/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace MinecraftSite
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
