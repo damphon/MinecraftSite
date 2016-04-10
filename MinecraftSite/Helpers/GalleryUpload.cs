139c849862aea9796a9cd776cb1bfd0fa26d1dc5
Jeremy <waddledoocanto@gmail.com>
2016-04-10 14:09:38 -0700

    Added uploads

--- MinecraftSite/Helpers/GalleryUpload.cs
+++ MinecraftSite/Helpers/GalleryUpload.cs
@@ -15,24 +15,43 @@ namespace MinecraftSite.Helpers
 
         public string ImageUpload(HttpPostedFileBase file)
         {
-            if (file != null && file.ContentLength > 0)
-                try
-                {
+            if ((file != null) && (file.ContentLength > 0) && (file.ContentLength <= 8000000))
+                try{
                     string path = Path.Combine(HttpContext.Current.Server.MapPath("~/Content/GalleryImages"), Path.GetFileName(file.FileName));
+                    string ext = Path.GetExtension(path);
+
+                    if (FileTypeIsImage(ext))
+                    {
+                        //MakeThumb();
+                    }else {
+                        ResultText = "Please use a png, jpg, bmp, or tiff file";
+                        return ResultText;
+                    }
                     file.SaveAs(path);
                     ResultText = "Image has been uploaded";
-
-                }
-                catch (Exception ex)
-                {
+                }catch (Exception ex){
                     ResultText = "Error: " + ex.Message.ToString();
-                }
-            else
-            {
-                ResultText = "Need File to upload";
+                }else{
+                ResultText = "Invalid Image Size";
             }
 
             return ResultText;
+        }
+
+        private void MakeThumb()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool FileTypeIsImage(string ext)
+        {
+            string type = ext.ToLower();
+            if ((type == ".png") || (type == ".jpeg") || (type == ".jpg") || (type == ".bmp") || (type == ".gif") || (type == ".tif") || (type == ".tiff")){
+                return true;
+            } else {
+                return false;
+            }
+            
         }
     }
 }
