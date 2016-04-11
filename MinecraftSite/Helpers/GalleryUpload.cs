2fe90b4ca4f4c56234a460d7878066b877dfef5b
Jeremy <waddledoocanto@gmail.com>
2016-04-11 08:03:58 -0700

    Upload now creates a thumbnale image

--- MinecraftSite/Helpers/GalleryUpload.cs
+++ MinecraftSite/Helpers/GalleryUpload.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net.Security;
@@ -16,31 +19,72 @@ namespace MinecraftSite.Helpers
         public string ImageUpload(HttpPostedFileBase file)
         {
             if ((file != null) && (file.ContentLength > 0) && (file.ContentLength <= 8000000))
-                try{
+                try
+                {
                     string path = Path.Combine(HttpContext.Current.Server.MapPath("~/Content/GalleryImages"), Path.GetFileName(file.FileName));
                     string ext = Path.GetExtension(path);
 
                     if (FileTypeIsImage(ext))
                     {
-                        //MakeThumb();
-                    }else {
+                        if (MakeThumb(file))
+                        {
+                            //Do sothething here
+                        }
+                        else
+                        {
+                            return ResultText;
+                        }
+                    }
+                    else
+                    {
                         ResultText = "Please use a png, jpg, bmp, or tiff file";
                         return ResultText;
                     }
                     file.SaveAs(path);
                     ResultText = "Image has been uploaded";
-                }catch (Exception ex){
+                }
+                catch (Exception ex){
                     ResultText = "Error: " + ex.Message.ToString();
-                }else{
+                }
+            else
+            {
                 ResultText = "Invalid Image Size";
             }
 
             return ResultText;
         }
 
-        private void MakeThumb()
+        private bool MakeThumb(HttpPostedFileBase file)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var filename = Path.GetFileName(file.FileName);
+                string path = Path.Combine(HttpContext.Current.Server.MapPath("~/Content/GalleryImages/thumbs"), filename); //Get path to save image to
+                Image sourceimage = Image.FromStream(file.InputStream);
+                int newWidth = 122;
+                int newHeight = 65;
+                var bitmap = new Bitmap(newWidth, newHeight);
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.SmoothingMode = SmoothingMode.HighSpeed;
+                    g.PixelOffsetMode = PixelOffsetMode.HighSpeed;
+                    g.CompositingQuality = CompositingQuality.HighSpeed;
+                    g.InterpolationMode = InterpolationMode.Bicubic;
+                    g.DrawImage(sourceimage, new Rectangle(0, 0, newWidth, newHeight));
+                }
+                ImageCodecInfo jpgInfo = ImageCodecInfo.GetImageEncoders().Where(codecInfo => codecInfo.MimeType == "image/jpeg").First();
+                using (EncoderParameters encParams = new EncoderParameters(1))
+                {
+                    encParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 80L);
+                    bitmap.Save(path, jpgInfo, encParams);
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultText = "Failed to create thumbnail " + ex.Message.ToString();
+                return false;
+            }
+            return true;
         }
 
         private bool FileTypeIsImage(string ext)
@@ -48,7 +92,9 @@ namespace MinecraftSite.Helpers
             string type = ext.ToLower();
             if ((type == ".png") || (type == ".jpeg") || (type == ".jpg") || (type == ".bmp") || (type == ".gif") || (type == ".tif") || (type == ".tiff")){
                 return true;
-            } else {
+            }
+            else
+            {
                 return false;
             }
             
