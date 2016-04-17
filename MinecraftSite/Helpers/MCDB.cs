16dd6dd9337aa30cab5714237e77262d5a9877ba
Jeremy <waddledoocanto@gmail.com>
2016-04-17 10:12:11 -0700

    Added date time to gallery uploads

--- MinecraftSite/Helpers/MCDB.cs
+++ MinecraftSite/Helpers/MCDB.cs
@@ -11,6 +11,7 @@ namespace MinecraftSite.Helpers
 {
     public class MCDB
     {
+        /////////////////////////////////////////////////////////////DB querys for Gallery go here////////////////////////////////////////////////////////////////////
         public bool NewGalleryImage(string filename, string username, string description)
         {
             using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MCDBConnection"].ToString()))
@@ -52,6 +53,7 @@ namespace MinecraftSite.Helpers
                         while (reader.Read())
                         {
                             var GalleryString = new GalleryModel();
+                            GalleryString.CommentDateQuery = Convert.ToDateTime(reader["CommentDate"]).ToString("MMMM dd, yyyy H:mm");
                             GalleryString.FileNameQuery = reader.GetString(reader.GetOrdinal("FileName"));
                             GalleryString.UserNameQuery = reader.GetString(reader.GetOrdinal("UserName"));
                             GalleryString.DescriptionQuery = reader.GetString(reader.GetOrdinal("Description"));
@@ -62,6 +64,33 @@ namespace MinecraftSite.Helpers
                 }
             }
             return ListOfStrings;
-        } 
+        }
+        /////////////////////////////////////////////////////////////DB querys for Comments go here////////////////////////////////////////////////////////////////////
+        public List<CommentModel> GetComments()
+        {
+            var ListOfStrings = new List<CommentModel>();
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MCDBConnection"].ToString()))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Comments";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var CommentString = new CommentModel();
+                            CommentString.CommentDateQuery = String.Format("{0:u}", reader.GetString(reader.GetOrdinal("CommentDate")));
+                            CommentString.PageQuery = reader.GetString(reader.GetOrdinal("Page"));
+                            CommentString.UserNameQuery = reader.GetString(reader.GetOrdinal("UserName"));
+                            CommentString.CommentQuery = reader.GetString(reader.GetOrdinal("Comment"));
+
+                            ListOfStrings.Add(CommentString);
+                        }
+                    }
+                }
+            }
+            return ListOfStrings;
+        }
     }    
 }
