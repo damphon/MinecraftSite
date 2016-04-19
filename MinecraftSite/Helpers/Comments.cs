04e741be14340d57bcfb08f39b7ffb84bf1ceab2
Jeremy <waddledoocanto@gmail.com>
2016-04-19 10:14:32 -0700

    Comment section done, needs styling

--- MinecraftSite/Helpers/Comments.cs
+++ MinecraftSite/Helpers/Comments.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MinecraftSite.Models;
+using System.Text;
 
 namespace MinecraftSite.Helpers
 {
@@ -23,6 +25,31 @@ namespace MinecraftSite.Helpers
             string[] whitelist = new string[] { "damphon", "clouddesu", "garangatang", "sterlingwing", "teepek", "talerdyn", "biscuitdesucre", "majesticbrother", "covolt100" };
             if (whitelist.Any(UserName.ToLower().Contains)) return true;
             else return false;
+        }
+
+        public string ArrayBuilder(string CurrentURL)
+        {
+            var CommentString = CommentModel.CommentHTML();
+            string tempstring = "";
+
+            foreach (var comment in CommentString)
+            {
+                if (comment.PageQuery == CurrentURL)
+                {
+                    StringBuilder sb = new StringBuilder(tempstring);
+                    sb.Append("<li class='comment'><div class='commentHeader'><cite><img src = 'https://minotar.net/helm/");
+                    sb.Append(comment.UserNameQuery);
+                    sb.Append("/150.png' width='30' Height='30'/><span class= 'commentUser'>");
+                    sb.Append(comment.UserNameQuery);
+                    sb.Append("</span><span class='commentTime'>");
+                    sb.Append(comment.CommentDateQuery);
+                    sb.Append("</span></cite ></div ><div class='commentBody'><div class='commentMessage'><p>");
+                    sb.Append(comment.CommentQuery);
+                    sb.Append("</p></div></div></li>");
+                    tempstring = sb.ToString();
+                }
+            }
+            return tempstring;
         }
     }
 }
